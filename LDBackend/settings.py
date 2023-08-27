b91500cf1999c5cdebade92630664202130ca263
Parth Patel <58204750+john-patel@users.noreply.github.com>
2023-08-27 11:06:11 +0530

    Update settings.py

--- LDBackend/settings.py
+++ LDBackend/settings.py
@@ -139,7 +139,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # use 587 for local use and 465 for online app
 
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 465
+EMAIL_PORT = 587
 EMAIL_HOST_USER = 'parthpatelaa19@gmail.com'
 EMAIL_HOST_PASSWORD = 'mxqslrkvudbtjdxq'
 EMAIL_USE_TLS = True
